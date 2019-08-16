3a1419e26f4a92d2e7d62cd8f9ba784e4b59578b
Giuliana Martini <yulimartini94@gmail.com>
2019-08-15 21:03:19 -0300

    Ejercicios N°1 y N° 2 resueltos

--- Lab de Computación 2/src/trabajoPráctico2/AlmacenarenArrays.java
+++ Lab de Computación 2/src/trabajoPráctico2/AlmacenarenArrays.java
@@ -1,26 +1,46 @@
 package trabajoPráctico2;
 
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class AlmacenarenArrays {
+	
+/*Ejercicio 1: Almacenar en Array
+Desarrollar un programa que pida al usuario que introduzca 10 números enteros por consola.
+Los números introducidos se almacenarán en un array. 
+El programa imprime en consola cuántos números negativos, positivos y valores 0 hay en el array.*/
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		int enteros[] = new int [10];
 				
-		for (int i=0; i < enteros.length; i++) {
-			enteros[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un número entero: "));
-			while(i % 1 != 0) {
-				JOptionPane.showMessageDialog(null, "El número ingresado no es un entero.", "ADVERTENCIA", JOptionPane.ERROR_MESSAGE);
-				enteros[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un número entero: "));
+		int enteros[] = new int [10];
+		int positivos = 0; 
+		int negativos = 0;
+		int ceros = 0;
+			
+		//mensaje que se muestra en cosola
+		System.out.println("Por favor, ingrese los números enteros:");
+		Scanner input = new Scanner(System.in);
+		
+		//bucle que toma los datos ingresados por el usuario y los almacena en el array
+		for(int i=0; i<10; i++) {					
+			System.out.println("Ingrese el número para la posición "+(i+1)+":");
+			enteros[i] = input.nextInt();
+		}
+		input.close();
+		
+		//éste bucle recorre el array y evalúa los datos que se almacenarán en las variables
+		for (int entero:enteros) {
+			if(entero > 0) {
+				positivos++;
+			}else if(entero < 0) {
+				negativos++;
+			}else {
+				ceros++;
 			}
 		}
-		
-		for (int entero:enteros) {
-			System.out.print(entero);
-		}
-
+		//mostrar los resultados por consola
+		System.out.print("Ha ingresado una cantidad de "+ceros+ " ceros, "+ positivos+" números positivos y "+negativos+" números negativos");
 	}
-
+		
 }
+
+
