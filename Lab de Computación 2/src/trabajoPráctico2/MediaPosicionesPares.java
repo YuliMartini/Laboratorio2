a97f7d875e843be03db256c1f6aea92438b93705
Giuliana Martini <yulimartini94@gmail.com>
2019-08-25 01:39:31 -0300

    Revert "Update MediaPosicionesPares.java"
    This reverts commit 0f76996f2edd7da6188df84d94e83e1602f00f93.

--- Lab de Computación 2/src/trabajoPráctico2/MediaPosicionesPares.java
+++ Lab de Computación 2/src/trabajoPráctico2/MediaPosicionesPares.java
@@ -1,50 +1,12 @@
 package trabajoPráctico2;
 
-import java.util.Scanner;
-
 public class MediaPosicionesPares {
-	
-	/*Ejercicio 2: Media en posiciones pares del Array
-      En este ejercicio, se pide al usuario que introduzca 10 números enteros por consola. 
-      Los números se almacenarán en un array y el programa calculará la media de los números 
-      introducidos en las posiciones pares del array. (Debes utilizar el operador módulo “%”).*/
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		
-		int enteros[]= new int[10];
-		int pares = 0;
-		int media = 0;
 		
-		System.out.println("Por favor, ingrese los números enteros:");
-		
-		Scanner input = new Scanner(System.in);		
-		
-		for(int i=0; i<10; i++) {					
-			System.out.println("Ingrese el número para la posición "+(i+1)+":");
-			enteros[i] = input.nextInt();
-		}
-		input.close();
-		
-		/*for(int i=0; i<10; i++) {
-			System.out.println("Ingrese el número"+i);
-			enteros[i] = input.nextInt();
-		}*/
-		
-		for (int entero:enteros) {
-			if(i%2 == 0 && i != 0) {
-				pares++;
-				media = media + enteros[i];					
-			}
-		}
-		
-		/*for(int i=0; i<enteros.length; i++) {		
-			if(i%2 == 0 && i != 0) {
-				pares++;
-				media = media + enteros[i];					
-			}
-		}*/
-				
-		System.out.print("La media de las posiciones pares del array es: "+media/pares);
+
 	}
-	
+
 }
